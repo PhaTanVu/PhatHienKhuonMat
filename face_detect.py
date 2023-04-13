ac18499eac41066d9e85af58d16bb5768ad46a59
Phan Tan Vu <125203748+PhaTanVu@users.noreply.github.com>
2023-04-13 15:18:06 +0700

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,18 +1,27 @@
+import streamlit as st
 import cv2
 
-cap = cv2.VideoCapture(0) # 0 là ID của camera mặc định
+cap = cv2.VideoCapture(0)
 
-while True:
-    # Lấy từng frame của camera
+if not cap.isOpened():
+    st.error("Cannot open camera")
+
+stop_button = st.button("Stop")
+while not stop_button:
     ret, frame = cap.read()
-
-    # Hiển thị frame lên màn hình
-    cv2.imshow('Camera', frame)
-
-    # Nếu nhấn phím 'q' thì thoát khỏi vòng lặp
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if not ret:
+        st.error("Failed to grab frame")
         break
 
-# Giải phóng tài nguyên và đóng cửa sổ hiển thị hình ảnh
+    cv2.imshow('Camera', frame)
+
+    # Wait for a key press to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    # Update the stop button state
+    stop_button = st.session_state.stop_button
+
+# Clean up
 cap.release()
 cv2.destroyAllWindows()
