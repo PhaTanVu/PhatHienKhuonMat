87d51166c36665676c26f396975d9153bbb06d10
Phan Tan Vu <125203748+PhaTanVu@users.noreply.github.com>
2023-04-13 15:15:32 +0700

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -1,54 +1,18 @@
-import streamlit as st
-import numpy as np
-import cv2 as cv
+import cv2
 
-st.subheader('Phát hiện khuôn mặt')
-FRAME_WINDOW = st.image([])
-cap = cv.VideoCapture(0) # Set up camera capture with default device ID
-
-def visualize(input, faces, fps, thickness=2):
-    if faces[1] is not None:
-        for idx, face in enumerate(faces[1]):
-            # print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
-
-            coords = face[:-1].astype(np.int32)
-            cv.rectangle(input, (coords[0], coords[1]), (coords[0]+coords[2], coords[1]+coords[3]), (0, 255, 0), thickness)
-            cv.circle(input, (coords[4], coords[5]), 2, (255, 0, 0), thickness)
-            cv.circle(input, (coords[6], coords[7]), 2, (0, 0, 255), thickness)
-            cv.circle(input, (coords[8], coords[9]), 2, (0, 255, 0), thickness)
-            cv.circle(input, (coords[10], coords[11]), 2, (255, 0, 255), thickness)
-            cv.circle(input, (coords[12], coords[13]), 2, (0, 255, 255), thickness)
-    cv.putText(input, 'FPS: {:.2f}'.format(fps), (1, 16), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-detector = cv.FaceDetectorYN.create(
-    'face_detection_yunet_2022mar.onnx',
-    "",
-    (320, 320),
-    0.9,
-    0.3,
-    5000
-)
-
-tm = cv.TickMeter()
-frameWidth = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-detector.setInputSize([frameWidth, frameHeight])
+cap = cv2.VideoCapture(0) # 0 là ID của camera mặc định
 
 while True:
-    hasFrame, frame = cap.read()
-    if not hasFrame:
-        print('No frames grabbed!')
+    # Lấy từng frame của camera
+    ret, frame = cap.read()
+
+    # Hiển thị frame lên màn hình
+    cv2.imshow('Camera', frame)
+
+    # Nếu nhấn phím 'q' thì thoát khỏi vòng lặp
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    frame = cv.resize(frame, (frameWidth, frameHeight))
-
-    # Inference
-    tm.start()
-    faces = detector.detect(frame) # faces is a tuple
-    tm.stop()
-
-    # Draw results on the input image
-    visualize(frame, faces, tm.getFPS())
-
-    # Visualize results
-    FRAME_WINDOW.image(frame, channels='BGR')
+# Giải phóng tài nguyên và đóng cửa sổ hiển thị hình ảnh
+cap.release()
+cv2.destroyAllWindows()
